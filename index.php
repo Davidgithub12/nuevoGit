4c4375bf6cde1d326d27b6731774d739b3933ad2
Davidgithub12 <david1884@outlook.es>
2022-07-01 08:44:12 -0500

    Ultimo trabajo

--- index.php
+++ index.php
@@ -1,83 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="bootstrap.min (6).css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>w
-	<script>
-		$(document).on("click","#btn-publicar",()=>{
-			const user=$("#pub_usuario").val();
-			const desc=$("#pub_descripcion").val();
-			const img=$("#pub_imagen").val();
-			const est=$("#pub_estado").val();
-			$.ajax({
-				url:'acciones_publicaciones.php',
-				data:{user:user,desc:desc,img:img,est:est},
-				type:'POST',
-				dataType:'json',
-				success:(data)=>{
-					console.log(data);
-					$("#estado").text(data[0].pub_estado);
-					$("#publicaciones").text(data[0].pub_descripcion);
-					if(data[0].pub_estado=="Hail Hittler") {
-						$(".cont_publicacion").removeClass("bg-primary");
-						$(".cont_publicacion").removeClass("bg-danger");
-						$(".cont_publicacion").removeClass("bg-info");
-						$(".cont_publicacion").removeClass("bg-succes");
-					}
-					if(data[0].pub_estado=="Troste unu") {
-						$(".cont_publicacion").removeClass("bg-primary");
-						$(".cont_publicacion").removeClass("bg-danger");
-						$(".cont_publicacion").removeClass("bg-info");
-						$(".cont_publicacion").removeClass("bg-succes");
-					}
-					if(data[0].pub_estado=="Enojao ùnú") {
-						$(".cont_publicacion").removeClass("bg-primary");
-						$(".cont_publicacion").removeClass("bg-danger");
-						$(".cont_publicacion").removeClass("bg-info");
-						$(".cont_publicacion").removeClass("bg-succes");
-					}
-				},
-				error:(desc,estado)=>{},
-			})
-
-		 });
-	</script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>    
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" /> 
+    <script>
+     $(document).on("click","#btn_publicar",()=>{
+         const user=$("#pub_usuario").val();
+         const desc=$("#pub_descripcion").val();
+         const estado=$("#pub_estado").val();
+         $.ajax({
+            url:'acciones_publicaciones.php',
+            data:{user:user,desc:desc,estado:estado},
+            type:'POST',
+            dataType:'json',
+            success:(data)=>{
+                console.log(data);
+                $("#aux_id").val(data[0].pub_id);
+                $("#estado").text(data[0].pub_estado);
+                $("#publicacion").text(data[0].pub_descripcion);
+                if(data[0].pub_estado=='Alegre'){
+                   $(".cont_publicacion").removeClass("bg-info");
+                   $(".cont_publicacion").removeClass("bg-danger");
+                   $(".cont_publicacion").addClass("bg-success");
+                }
+                if(data[0].pub_estado=='Triste'){
+                   $(".cont_publicacion").removeClass("bg-success");
+                   $(".cont_publicacion").removeClass("bg-danger");
+                   $(".cont_publicacion").addClass("bg-info");
+                }
+                if(data[0].pub_estado=='Molesto'){
+                   $(".cont_publicacion").removeClass("bg-success");
+                   $(".cont_publicacion").removeClass("bg-info");
+                   $(".cont_publicacion").addClass("bg-danger");
+                }
+            },error:(desc,estado)=>{
+                alert("Un error ha sucedido"+estado);
+            },
+         })
+     });
+    </script>
 </head>
-<body>
-	<h1 class="title title-success"> VN Book</h1>
-	<div class="container">
-
-	  <div class="row">
-	  	<div class="col-md-6">
-	  		<input type="text" placeholder="Usuario" class="form-control" id="pub_usuario">
-			<textarea id="pub_descripcion" cols="30" rows="2" class="form-control"></textarea>
-			<input type="file" id="pub_imagen" class="form-control">	
-	  		<select id="pub_estado" class="form-control bg-dark">
-			<option value="">Elija una opción</option>
-			<option value="Hail Hittler">Hail Hittler</option>
-			<option value="Troste unu">Troste unu</option>
-			<option value="Enojao ùnú">Enojao ùnú</option>
-		</select>
-		<div class="d-grid gap-2">
-				<button class="btn btn-danger" id="btn-publicar">Publicar</button>
-			</div>
-	  	</div>
-
-	  	<div class="col-md-6">
-	  		<img src="no_imagen.png" width="220px" alt="">
-	  	</div>
-	  </div>
-	  
-	  <div class="row">
-	  	<div class="card" style="margin: 20px;width: 10cm;">
-	  		<img src="a.jpg" class="card-img-top">
-	  	<h3 id="estado">Estado</h3>
-	  	<p class="card-text" id="publicacion">Descripcion</p>
-	  	</div>
-	  </div>		
-	</div>
+<body class="bg-dark">
+    <h1 class="bg-black" style="color: white;">VNBOOK - David Gomez</h1>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+            <input type="text" id="pub_usuario" placeholder="User" class="form-control">
+            <textarea  id="pub_descripcion" rows="2" class="form-control" placeholder="Description"></textarea>
+            <select id="pub_estado" class="form-control bg-dark" style="color: white;">
+                <option value="">Select a option</option>
+                <option value="Alegre">Alegre</option>
+                <option value="Triste">Triste</option>
+                <option value="Molesto">Molesto</option>
+            </select>
+            <div class="d-grid gap-2">
+                <button class="btn btn-primary" id="btn_publicar" >Post</button>
+            </div>
+        </div>
+        <div class="col-md-6">
+          
+          <div id="pub_img" class="dropzone"></div>
+          <input type="hidden" id="aux_id" >
+        </div>
+      </div>
+    <br>
+    <div class="row">
+        <div class="col-md-4">
+        <div class="card cont_publicacion"  style="width: 30rem;">
+              <img src="no_foto.jpg" class="card-img-top" id="aux_img" alt="...">
+              <div class="card-body">
+                    <h3 id="estado" style="color: dark;">Status</h3>
+                <p class="card-text" id="publicacion" style="color: black;" >Description</p>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
+<script>
+new Dropzone("#pub_img", {
+    url: 'acciones_publicaciones.php',
+    dictDefaultMessage: 'Select a image', 
+    paramName: "file", 
+    init: function () {
+        this.on("sending", function(file, xhr, formData){
+            formData.append("aux_id",$("#aux_id").val() );
+        });
+        this.on("success", function (file, data) {
+            $("#aux_img").attr('src',`img/${data}`)
+        });
+    }
+});
+</script>
